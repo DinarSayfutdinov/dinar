477cbf3090c2c3d8ac41420e78895ddb08fd20e0
gRanid <granid@granid-P5Q.(none)>
2012-04-22 15:34:06 +0400

    First Dinars commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinar::Application.config.secret_token = '6973d91407bca53fadd5d7e64b469ce445de5044eac154cf5062c528428612dc01898684556825eaba6c440ebafbdf3ebcd30694ae9ac7bafdd30aeaed92cf6f'
